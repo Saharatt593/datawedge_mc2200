4136223583a54a642764cdec911fd7e2c447f84b
SAHARATT593 <saharatt.s@cdg.co.th>
2021-05-20 14:57:53 +0700

    init

---
+++ app/src/main/java/com/saharatt593/dwmc2200/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.saharatt593.dwmc2200
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
